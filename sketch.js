01012f9f3025aea9e215abcafc434365d860da90
Bubadabado <adam@adamrobertson.us>
2025-12-09 09:48:56 -0700

    pre-refactor checkpoint

--- sketch.js
+++ sketch.js
@@ -32,9 +32,14 @@ let map_metadata = {
     }
 }; 
 let slopes = [];
-let dilated = [];
-let labels = [];
+let local_maxima = [];
+let local_minima = [];
+let labels_maxima = [];
+let labels_minima = [];
+let groups = [];
 let current_label = 1;
+
+let temp = []; //TODO remove
 
 const pixel_scale = 1.; //TODO implement properly with new layer system or remove
 
@@ -60,10 +65,10 @@ let ocean = {
 }
 
 let show_slope_overlay = false;
-let show_group_overlay = false;
+let show_group_overlay = true;
 let slope_overlay;
 let group_overlay;
-let overlay;
+let ocean_overlay;
 
 // =========================
 //   p5js-called functions
@@ -86,34 +91,78 @@ let overlay;
             y: (y) => 0 <= y && y < bounds.h,
             p(x, y) { return this.x(x) && this.y(y); },
         }
-        labels = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
-        console.log(labels)
+        labels_maxima = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
+        labels_minima = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
+        temp = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
+        groups = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
+        let colored = [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
 
         // initialize canvas layers
         createCanvas(bounds.w, bounds.h);
         bg = createGraphics(bounds.w, bounds.h);
+        bg.pixelDensity(1);
         bg.image(map, 0, 0);
         map.loadPixels();
         slope_overlay = createGraphics(bounds.w, bounds.h);
+        slope_overlay.pixelDensity(1);
         slope_overlay.clear();
         slope_overlay.loadPixels();
         group_overlay = createGraphics(bounds.w, bounds.h);
+        group_overlay.pixelDensity(1);
         group_overlay.clear();
         group_overlay.loadPixels();
-        overlay = createGraphics(bounds.w, bounds.h);
-        overlay.clear();
-        overlay.loadPixels();
+        ocean_overlay = createGraphics(bounds.w, bounds.h);
+        ocean_overlay.pixelDensity(1);
+        ocean_overlay.clear();
+        ocean_overlay.loadPixels();
 
         // perform per-pixel setup operations
         iterateMap((r, c) => {
             updatePuddle(r, c);
             setSlope(r, c);
-            dilate(r, c, 5);
+            setLocalMaxima(r, c);
+            setLocalMinima(r, c);
         });
-        labelCntdCmpts();
+        labelCntdCmpts(local_maxima, labels_maxima);
+        labelCntdCmpts(local_minima, labels_minima);
+        groupFeatures(labels_minima);
+        labelCntdCmpts(labels_minima, temp, 0);
 
+        //color local minima features
+        iterateMap((r, c) => {
+             if(temp[r][c] !== 0) {
+                const col = {
+                    r: 0,
+                    g: 0,
+                    b: 0,
+                    a: 128
+                };
+                colored[r][c] = 1;
+
+                writeToOverlay(r, c, col, group_overlay);
+            }
+        });
+
+        groupFeatures(labels_maxima, 2);
+        groupFeatures(local_minima, 2);
+        
+        //color local maxima features
+        iterateMap((r, c) => {
+             if(temp[r][c] !== 0 && colored[r][c] == 0) {
+                randomSeed(temp[r][c] * 65539);
+                const col = {
+                    r: random(0, 255),
+                    g: random(0, 255),
+                    b: random(0, 255),
+                    a: 80
+                };
+                writeToOverlay(r, c, col, group_overlay);
+            }
+        });
+
+        //update graphics buffers
         slope_overlay.updatePixels();
-        overlay.updatePixels();
+        ocean_overlay.updatePixels();
         group_overlay.updatePixels();
 
         // begin DOM update loop
@@ -129,7 +178,7 @@ let overlay;
             image(slope_overlay, 0, 0);
         if (show_group_overlay)
             image(group_overlay, 0, 0);
-        image(overlay, 0, 0);
+        image(ocean_overlay, 0, 0);
     }
 }
 
@@ -151,15 +200,15 @@ function getValue(r, c) {
 
 // update the ocean
 function updateOcean() {
-    overlay.clear();
-    overlay.loadPixels();
+    ocean_overlay.clear();
+    ocean_overlay.loadPixels();
     iterateMap(updatePuddle);
-    overlay.updatePixels();
+    ocean_overlay.updatePixels();
 }
 // update a single pixel in the ocean grid
 function updatePuddle(r, c) {
     if (ocean.threshold > getValue(r, c)) {
-        writeToOverlay(r, c, ocean.color, overlay);
+        writeToOverlay(r, c, ocean.color, ocean_overlay);
     }
 }
 
@@ -179,33 +228,39 @@ function setSlope(r, c) {
     }, slope_overlay);
 }
 
-//perform a dilation at r,c with kernel of shape n,n
-function dilate(r, c, n = 3) {
-    if (dilated[r] === undefined)
-        dilated[r] = [];
-    
-    const rad = Math.floor(n / 2);
-    let max = -Infinity;
-    for (let y = -rad; y <= rad; y++) {
-        for (let x = -rad; x <= rad; x++) {
-            if (check_bounds.p(c + x, r + y)) 
-                max = Math.max(max, getValue(r + y, c + x));
-        }
-    }
-    is_local_maxima = max == getValue(r, c);
-    dilated[r][c] = col = is_local_maxima ? max : -1;
-
-    writeToOverlay(r, c, {
-        r: col,
-        g: 0,
-        b: 0,
-        a: is_local_maxima ? 255 : 0
-    }, group_overlay);
+function setLocalMaxima(r, c) {
+    if (local_maxima[r] === undefined)
+        local_maxima[r] = [];
+    local_maxima[r][c] = findExtrema(r, c, 5);
 }
 
-function labelCntdCmpts() {
+function setLocalMinima(r, c) {
+    if (local_minima[r] === undefined)
+        local_minima[r] = [];
+    local_minima[r][c] = findExtrema(r, c, 5, false);
+}
+
+// find local extrema at r,c with kernel of shape n,n
+function findExtrema(r, c, n = 3, max = true) {
+    const rad = Math.floor(n / 2);
+    let extrema = getValue(r, c);
+    const traverse = max ? Math.max : Math.min
+
+    for (let y = -rad; y <= rad; y++) 
+        for (let x = -rad; x <= rad; x++) 
+            if ((x !== 0 || y !== 0) && check_bounds.p(c + x, r + y)) 
+                extrema = traverse(extrema, getValue(r + y, c + x));
+
+    is_local_extrema = extrema == getValue(r, c);
+    return is_local_extrema ? extrema : false;
+}
+// Combine connected components into labels via stack floodfill
+// from and to are arrays of the same shape as the map
+// from should either contain false or some value
+function labelCntdCmpts(from, to, def = false) {
+    // to ??= [...Array(bounds.h)].map(() => Array(bounds.w).fill(0));
     iterateMap((r, c) => {
-        if (dilated[r][c] !== -1 && labels[r][c] == 0) {
+        if (from[r][c] !== def && to[r][c] == 0) {
             let stack = [];
             stack.push({
                 r: r,
@@ -213,13 +268,13 @@ function labelCntdCmpts() {
             });
             while (stack.length > 0) {
                 p = stack.pop();
-                if (labels[p.r][p.c] == 0) {
-                    labels[p.r][p.c] = current_label;
+                if (to[p.r][p.c] == 0) {
+                    to[p.r][p.c] = current_label;
                     for (let rr = -1; rr <= 1; rr++) {
                         for (let cc = -1; cc <= 1; cc++) {
                             const r2 = p.r + rr;
                             const c2 = p.c + cc;
-                            if ((rr === 0 && cc === 0) || !check_bounds.p(c2, r2) || dilated[r2][c2] === -1)
+                            if ((rr === 0 && cc === 0) || !check_bounds.p(c2, r2) || from[r2][c2] === def)
                                 continue;
                             stack.push({ r: r2, c: c2 });
                         }
@@ -231,6 +286,98 @@ function labelCntdCmpts() {
     });
 }
 
+//TODO fix and document this mess
+function groupFeatures(arr, n = 1) {
+    // margin of error; 0.0 means that the floodfill will only go downhill, whereas 
+    // any value above that will allow for travelling up to that value in m upwards.
+    // Low values may lead to some 'holes' in groups where terrain is noisy 
+    const MOE = 0.0; 
+    const DELTA_SLOPE = 5;
+    let DELTA_ELEV = 4; //dark is smooth at 4m TODO (range)
+    
+    
+    const fn = () => {
+        const queue = [];
+        let qi = 0; 
+        iterateMap((r, c) => {
+            if (arr[r][c] !== 0) {
+                queue.push({r, c}); 
+            }
+        });
+
+        while (qi < queue.length) {
+            //reference point
+            const p = queue[qi++];
+            const group_label = arr[p.r][p.c];
+            
+            for (let rr = -1; rr <= 1; rr++) {
+                for (let cc = -1; cc <= 1; cc++) {
+                    //target coords
+                    const r2 = p.r + rr;
+                    const c2 = p.c + cc;
+
+                    if (false 
+                        || (rr === 0 && cc === 0) 
+                        || !check_bounds.p(c2, r2) 
+                        || arr[r2][c2] !== 0
+                        || abs(getElevAt(c2, r2) - getElevAt(p.c, p.r)) > DELTA_ELEV
+                        // || slopes[p.r][p.c] - slopes[r2][c2] > DELTA_SLOPE
+                    ) { continue; }
+                    
+                    queue.push({ r: r2, c: c2 });
+                    arr[r2][c2] = group_label;
+                }
+            }
+        }
+    }
+    
+    
+
+    const fn2 = () => {
+        const queue = [];
+        let qi = 0; 
+        iterateMap((r, c) => {
+            if (arr[r][c] !== 0 && temp[r][c] === 0) { //don't overwrite minima on plateaus
+                queue.push({r, c}); 
+            }
+        });
+
+        while (qi < queue.length) {
+            //reference point
+            const p = queue[qi++];
+            const group_label = arr[p.r][p.c];
+            temp[p.r][p.c] = group_label;
+            for (let rr = -1; rr <= 1; rr++) {
+                for (let cc = -1; cc <= 1; cc++) {
+                    //target coords
+                    const r2 = p.r + rr;
+                    const c2 = p.c + cc;
+
+                    if (false
+                        || (rr === 0 && cc === 0) 
+                        || !check_bounds.p(c2, r2) 
+                        || arr[r2][c2] !== 0
+                        || temp[r2][c2] !== 0
+                        || getElevAt(c2, r2) > getElevAt(p.c, p.r)
+                        // || slopes[p.r][p.c] - slopes[r2][c2] > DELTA_SLOPE
+                    ) { continue; }
+                    
+                    queue.push({ r: r2, c: c2 });
+                    arr[r2][c2] = group_label;
+                    temp[r2][c2] = group_label;
+                }
+            }
+        }
+    }
+
+    if (n == 1) {
+        fn();
+    } else {
+        fn2();
+    }
+}
+
+//write color col to buffer overlay at row r, col c
 function writeToOverlay(r, c, col, overlay) {
     const i = 4 * (r * map.width + c);
     overlay.pixels[i + 0] = col.r;
@@ -300,7 +447,7 @@ function updateDOM() {
     requestAnimationFrame(updateDOM);
 }
 
-// update helper functions
+// DOM update helper functions
 function updateCoords() {
     coords.textContent = `X: ${mouse.x}\nY: ${mouse.y}`;
 }
